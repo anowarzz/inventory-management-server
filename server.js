7f5e0ec0f4e2a96236e49416f3f2332814431a8e
anowarzz <anowarhosen444@gmail.com>
2023-03-26 10:34:13 +0600

    server initialized with express js

---
+++ server.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+// const DBConnect = require("./utils/dbConnect");
+
+const app = require("./app");
+
+
+
+
+
+// database connection
+// DBConnect();
+
+// server
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`.red.bold);
+});
+
