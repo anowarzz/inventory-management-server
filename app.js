9289a4f188ccd53b837d3182ba359c6986b106d5
anowarzz <anowarhosen444@gmail.com>
2023-03-29 10:06:16 +0600

    product schema design completed

--- app.js
+++ app.js
@@ -33,9 +33,54 @@ required: true ,
     unit: {
       type: String,
       required:true,
-      enum : ["kg", "litre", "pcs"]
-    }
-  
+      enum : {
+        value: ["kg", "litre", "pcs"],
+        message:"unit value can't be ${VALUE}, must be kg/litre/pcs"
+      }
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "Quantity can't be negative"],
+      validate: {
+        validator: (value) => {
+          const isInteger = Number.isInteger(value);
+          if(isInteger){
+            return true
+          }
+          else{
+            return false
+          }
+        }
+      },
+      message: "Quantity must be an integer" 
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["in-stock", "out-of-stock", "discontinued"],
+      message: "Status can't be ${VALUE}"
+    },
+    // createdAt: {
+    //   type: Date, 
+    //   default: Date.now
+    // },
+    // updatedAt: {
+    //   default: Date.now
+    // },
+    supplier: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Supplier"
+    },
+    categories: [{
+       name: {
+        type: String,
+        required: true,
+       },
+       _id: mongoose.Schema.Types.ObjectId
+    }]
+}, {
+  timestamps: true, 
 })
 
 
